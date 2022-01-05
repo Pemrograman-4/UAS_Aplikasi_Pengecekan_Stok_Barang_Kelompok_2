0f0a31b84ce528a59f9fa77cfdd73fe2e01e13f1
gilberthan242 <78791599+gilberthan242@users.noreply.github.com>
2022-01-05 12:12:29 +0700

    Delete Router directory

--- Router/image_router.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const router = require("express").Router();
-const controller = require("../Controller/image_controller");
-router.post("/upload", controller.upload);
-router.get("/files", controller.getListFiles);
-router.get("/files/:name", controller.download);
-module.exports = router;
